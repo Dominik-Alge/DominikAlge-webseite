28b9161287417d6ef87c4fd70dfa8fe97534ab84
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-16 15:50:23 +0200

    Create App component with multiple subcomponents

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Hero from "./components/Hero";
+import Problems from "./components/Problems";
+import Services from "./components/Services";
+import Approach from "./components/Approach";
+import WhyKinavita from "./components/WhyKinavita";
+import Team from "./components/Team";
+import References from "./components/References";
+import Process from "./components/Process";
+import Contact from "./components/contact";
+import WhoAreYou from "./components/WhoAreYou";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <WhoAreYou />
+      <Problems />
+      <Team />
+      <WhyKinavita />
+      <Services />
+      <References />
+      <Process />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
