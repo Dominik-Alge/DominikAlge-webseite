be2ff9034ad67c31ba0270860672e505100993ce
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-18 08:59:52 +0200

    Fix import statement for Contact component
    Correct import statement for Contact component to match file naming.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MyExperience from "./components/MyExperience";
 import Priorities from "./components/Priorities";
 import AboutMe from "./components/AboutMe";
 import Endorsements from "./components/Endorsements";
-import Contact from "./components/contact";
+import Contact from "./components/contact"; // FIX: Komplett kleingeschrieben wie deine Datei!
 import Datenschutz from "./components/Datenschutz";
 
 function App() {
