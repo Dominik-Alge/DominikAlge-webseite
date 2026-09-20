2cb44572d34b9a08ac891645a56a2a98d6988272
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-20 21:55:36 +0200

    Correct image paths in articles.js
    Updated image paths from '/public/' to '/images/' for consistency.

--- src/data/articles.js
+++ src/data/articles.js
@@ -2,15 +2,15 @@ export const articles = [
   {
     title: "Kein Vegan - Zwang",
     date: "16.09.2026",
-    image: "/public/vegan.png",
+    image: "/images/vegan.png", // KORREKTUR: Kein '/public', aber inklusive '/images'
     excerpt:
       "Warum wir die falsche Frage stellen.",
-    url: "https://www.linkedin.com/posts/dominik-alge_ernaeuhrungsinitiative-ernaeuhrungssicherheit-activity-7505727937179054080-Co8B?utm_source=social_share_send&utm_medium=android_app&rcm=ACoAACmMZdYBQTaVQYaHulMnOCnCJNWteeN-0U4&utm_campaign=copy_link",
+    url: "https://www.linkedin.com/posts/dominik-alge_ernaeuhrungsinitiative-ernaeuhrungssicherheit-activity-7505727937179054080-Co8B",
   },
   {
     title: "Fachkräfte sichern",
     date: "15.08.2026",
-    image: "/public/fachkraefte.jpg",
+    image: "/images/fachkraefte.jpg", // KORREKTUR: '/images/' statt '/public/'
     excerpt:
       "Was ich als Gruppenleiter täglich in der Industrie erlebe.",
     url: "https://www.linkedin.com/posts/dominik-alge_industrieforum-swissmanufacturing-industrie40-activity-7501549110190297090-0RdF",
@@ -18,7 +18,7 @@ export const articles = [
   {
     title: "Verantwortung übernehmen",
     date: "21.08.2026",
-    image: "/public/verantwortung.jpg",
+    image: "/images/verantwortung.jpg", // KORREKTUR: '/images/' statt '/public/'
     excerpt:
       "Warum pragmatische Politik wichtiger ist denn je.",
     url: "https://www.linkedin.com/posts/dominik-alge_diefalschefrage-problemeloesen-zukunftgestalten-activity-7496783306148073472-EW9O",
