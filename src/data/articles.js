e00ac23925e64dfac4db49578fdbc830d92fa153
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-20 21:35:35 +0200

    Update article images and URLs in articles.js

--- src/data/articles.js
+++ src/data/articles.js
@@ -2,15 +2,15 @@ export const articles = [
   {
     title: "Kein Vegan - Zwang",
     date: "16.09.2026",
-    image: "/images/berufsbildung.jpg",
+    image: "/public/vegan.jpg",
     excerpt:
       "Warum wir die falsche Frage stellen.",
-    url: "https://lnkd.in/p/egjiqKZG",
+    url: "https://www.linkedin.com/posts/dominik-alge_ernaeuhrungsinitiative-ernaeuhrungssicherheit-activity-7505727937179054080-Co8B?utm_source=social_share_send&utm_medium=android_app&rcm=ACoAACmMZdYBQTaVQYaHulMnOCnCJNWteeN-0U4&utm_campaign=copy_link",
   },
   {
     title: "Fachkräfte sichern",
     date: "15.08.2026",
-    image: "/images/fachkraefte.jpg",
+    image: "/public/fachkraefte.jpg",
     excerpt:
       "Was ich als Gruppenleiter täglich in der Industrie erlebe.",
     url: "https://www.linkedin.com/posts/dominik-alge_industrieforum-swissmanufacturing-industrie40-activity-7501549110190297090-0RdF",
@@ -18,7 +18,7 @@ export const articles = [
   {
     title: "Verantwortung übernehmen",
     date: "21.08.2026",
-    image: "/images/verantwortung.jpg",
+    image: "/public/verantwortung.jpg",
     excerpt:
       "Warum pragmatische Politik wichtiger ist denn je.",
     url: "https://www.linkedin.com/posts/dominik-alge_diefalschefrage-problemeloesen-zukunftgestalten-activity-7496783306148073472-EW9O",
