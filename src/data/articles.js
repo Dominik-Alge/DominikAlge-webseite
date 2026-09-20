3cdab045aba0ac561e1e35038c2a083d20602c00
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-20 23:11:18 +0200

    Remove duplicate article entry
    Removed duplicate article entry for 'Entscheidungsstunde für Deutschland'.

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,5 +1,13 @@
 export const articles = [
   {
+    title: "Entscheidungsstunde für Deutschland",
+    date: "20.09.2026",
+    image: "/Demokratie.png", // KORREKTUR: Großes 'D' beachten!
+    excerpt:
+      "Konkurrenzdemokratie am Ende: Warum wir nach den jüngsten Wahlergebnissen mehr Konsens und neue Wege der Koalitionsbildung wagen müssen.",
+    url: "https://www.linkedin.com/posts/dominik-alge_landtagswahl2026-demokratiewagen-sachpolitik-activity-7507545525542535169-X4hT",
+  },
+  {
     title: "Kein Vegan - Zwang",
     date: "16.09.2026",
     image: "/vegan.png", // KORREKTUR: Kein '/public', aber inklusive '/images'
@@ -22,13 +30,5 @@ export const articles = [
     excerpt:
       "Warum pragmatische Politik wichtiger ist denn je.",
     url: "https://www.linkedin.com/posts/dominik-alge_diefalschefrage-problemeloesen-zukunftgestalten-activity-7496783306148073472-EW9O",
-  },
-  {
-    title: "Entscheidungsstunde für Deutschland",
-    date: "20.09.2026",
-    image: "/Demokratie.png", // KORREKTUR: Großes 'D' beachten!
-    excerpt:
-      "Konkurrenzdemokratie am Ende: Warum wir nach den jüngsten Wahlergebnissen mehr Konsens und neue Wege der Koalitionsbildung wagen müssen.",
-    url: "https://www.linkedin.com/posts/dominik-alge_landtagswahl2026-demokratiewagen-sachpolitik-activity-7507545525542535169-X4hT",
   },
 ];
