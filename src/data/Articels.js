020e629b529f1d15c442f69f889882416912fbd1
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-20 20:30:01 +0200

    Add articles data to Articels.js

---
+++ src/data/Articels.js
@@ -0,0 +1,26 @@
+export const articles = [
+  {
+    title: "Kein Vegan - Zwang",
+    date: "16.09.2026",
+    image: "/images/berufsbildung.jpg",
+    excerpt:
+      "Warum wir die falsche Frage stellen.",
+    url: "https://lnkd.in/p/egjiqKZG",
+  },
+  {
+    title: "Fachkräfte sichern",
+    date: "15.08.2026",
+    image: "/images/fachkraefte.jpg",
+    excerpt:
+      "Was ich als Gruppenleiter täglich in der Industrie erlebe.",
+    url: "https://www.linkedin.com/posts/DEIN-ARTIKEL-2",
+  },
+  {
+    title: "Verantwortung übernehmen",
+    date: "08.07.2026",
+    image: "/images/verantwortung.jpg",
+    excerpt:
+      "Warum pragmatische Politik wichtiger ist denn je.",
+    url: "https://www.linkedin.com/posts/DEIN-ARTIKEL-3",
+  },
+];
